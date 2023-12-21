bbeaa3b210303923cbda7e77e96bf52989982185
mintcc342g <52809740+mintcc342g@users.noreply.github.com>
2023-12-21 15:51:06 +0900

    add signUp service

---
+++ controllers/dtos/dtos.go
@@ -0,0 +1,20 @@
+package dtos
+
+import (
+	"study-kafka-ddb/domains/enums"
+	"time"
+)
+
+type SignUpReq struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type SignUpResp struct {
+	ID        enums.UserID `json:"id"`
+	CreatedAt time.Time    `json:"created_at"`
+	UpdatedAt time.Time    `json:"updated_at"`
+	Name      string       `json:"name"`
+	Email     string       `json:"email"`
+}
