2c68dfae60c5014155d74f6fa3ae871590c6e6bc
brielle <brielle.choung@groundx.xyz>
2024-01-02 16:33:40 +0900

    add seek position in service

--- services/matching_service.go
+++ services/matching_service.go
@@ -43,3 +43,36 @@ func (r *MatchingService) OpenPosition(ctx context.Context, req *dtos.OpenPositi
 		IsOpened:  wanted.IsOpened,
 	}, nil
 }
+
+func (r *MatchingService) SeekPosition(ctx context.Context, req *dtos.SeekPositionReq) (*dtos.SeekPositionResp, deftype.Error) {
+	user, err := r.userRepo.Get(ctx, req.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = r.postRepo.GetUserPostByPositionAndGenre(ctx, user.ID, req.Position, req.FavoriteGenre)
+	if err == nil {
+		return nil, deftype.ErrDuplicatedRequest
+	} else if !err.Equal(deftype.ErrNotFound) {
+		return nil, err
+	}
+
+	resume, err := user.SeekPosition(req.Contents, req.Position, req.FavoriteGenre)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = r.postRepo.Save(ctx, resume); err != nil {
+		return nil, err
+	}
+
+	return &dtos.SeekPositionResp{
+		ID:            resume.ID,
+		FavoriteGenre: resume.FavoriteGenre,
+		CreatedAt:     resume.CreatedAt,
+		UpdatedAt:     resume.UpdatedAt,
+		Position:      resume.Position,
+		Contents:      resume.Contents,
+		IsOpened:      resume.IsOpened,
+	}, nil
+}
