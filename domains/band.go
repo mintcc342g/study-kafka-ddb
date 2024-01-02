dd7df59ebddb0f3adff823ac92f8cc07791f5575
mintcc342g <52809740+mintcc342g@users.noreply.github.com>
2024-01-02 13:35:13 +0900

    implement openPosition

--- domains/band.go
+++ domains/band.go
@@ -2,6 +2,7 @@ package domains
 
 import (
 	"study-kafka-ddb/domains/enums"
+	"study-kafka-ddb/utils/deftype"
 	"time"
 )
 
@@ -13,4 +14,20 @@ type Band struct {
 	ReaderID  enums.UserID
 	MemberNum int
 	Genres    []enums.Genre
+}
+
+func (r *Band) OpenPosition(user *User, position enums.BandPosition, contents string) (*Post, deftype.Error) {
+	if !r.CheckReader(user) {
+		return nil, deftype.ErrUnauthorized
+	}
+
+	if !isValidContents(contents) {
+		return nil, deftype.ErrInvalidRequestData
+	}
+
+	return NewWantedPost(user.ID, contents, position, r.ID), nil
+}
+
+func (r *Band) CheckReader(user *User) bool {
+	return r.ReaderID == user.ID
 }
